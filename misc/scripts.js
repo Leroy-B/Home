9068d4b405fa955e0c69dd362dc9fca7161befdb
Leroy <leroy.bonventre@icloud.com>
2018-05-19 11:16:13 +0200

    update

--- misc/scripts.js
+++ misc/scripts.js
@@ -26,9 +26,6 @@ $(document).ready(function(){
         case 'orange':
             $(getElementsById("hashrate currency wallet")).css("color", "orange");
             break;
-        default:
-            $(getElementsById("hashrate currency wallet")).css("color", "black");
-            break;
     }
     
     var xmrURL = 'img/xmr.png';
@@ -57,12 +54,6 @@ $(document).ready(function(){
             $("#currency").css("padding-left", "18px");
             //$('currency').css('background', 'url("' + btcURL + '") 0 2px no-repeat');
             break;
-        default:
-            $("#currency").css("background", "url('"+xmrURL+"') 0 2px no-repeat");
-            $("#currency").css("background-size", "16px 16px");
-            $("#currency").css("padding-left", "18px");
-            //$('currency').css('background', 'url("' + btcURL + '") 0 2px no-repeat');
-            break;
     }
     
     var progressbarColor;
@@ -77,9 +68,6 @@ $(document).ready(function(){
             break;
         case 'orange':
             $(".progress-bar").css("background", "#ff8000");
-            break;
-        default:
-            $(".progress-bar").css("background", "#000000");
             break;
     }
     
