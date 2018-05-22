946acbb2164d8b1240584389ccbabc2dd0818db6
Leroy <student@Urss-Mac-mini.local>
2018-05-22 16:02:23 +0200

    major update, release 2 packages

---
+++ pack.sh
@@ -0,0 +1,12 @@
+rm -r *.bz2
+cd deb
+rm -r deb/*.deb
+cd ../projects
+
+dpkg-deb -b -Zgzip "CustomCarrier"
+dpkg-deb -b -Zgzip "NoPasscodeButtonLightup"
+
+mv *.deb ../deb
+cd ..
+dpkg-scanpackages -m . /dev/null > Packages
+bzip2 Packages
